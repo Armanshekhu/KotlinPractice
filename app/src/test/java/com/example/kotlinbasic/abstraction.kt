c4d24d704f110840b9e1687b54c2ece343a3a546
Arman Shekhu <armanshekhu786@gmail.com>
2026-07-14 07:38:32 +0530

    Understanding Abstraction

--- app/src/test/java/com/example/kotlinbasic/abstraction.kt
+++ app/src/test/java/com/example/kotlinbasic/abstraction.kt
@@ -2,6 +2,11 @@ package com.example.kotlinbasic
 
 fun main() {
 
+    val array = arrayOf(Triangles(),Circles(),Picture())
+    for(shape in array) {
+        shape.name()
+    }
+
 }
 // you can have abstract function as well as abstract variable
 // abstract class are by default considered open
@@ -9,13 +14,27 @@ fun main() {
 // you can have more than one abstract function in a class
 // Meaning of abstract - If I don't know the value of a variable or implementation of a function,
 // but we know it subclasses will implement as use it according to them so we make them abstract
-abstract class Shapes {
+
+// Multiple interfaces can also be used to mimic multiple inheritance
+abstract class Shapes:Name,Star {
     abstract var i : Int
     abstract fun draw()
     abstract fun side()
-    fun name() {
-        println("Name is Shapes")
+}
+
+// function inside interface are abstract my default
+//now name function can be used across two separate inheritance tree
+interface Name {
+//  abstract function as no-body
+    fun name()
+//  open function can also be implemented
+    fun xyz() {
+        println("xyz")
     }
+}
+
+interface Star {
+
 }
 
 class Circles:Shapes() {
@@ -25,6 +44,9 @@ class Circles:Shapes() {
     }
     override fun side() {
         println("Circle has $i sides")
+    }
+    override fun name() {
+        println("Name is circle")
     }
 }
 
@@ -36,4 +58,16 @@ class Triangles:Shapes() {
     override fun side() {
         println("Triangle has $i sides")
     }
+
+    override fun name() {
+        println("Name is triangle")
+    }
+}
+
+
+class Picture:Name {
+    override fun name() {
+        println("Name is Picture")
+    }
+
 }
